0e4a9a2811611e6fee08c79d7fedb929de259dea
Tainara Matusiak <taainaramoraes@gmail.com>
2025-01-15 11:02:15 -0300

    Update sair do sistema
    Update opção de sair do sistema

--- Cartório com switch.cpp
+++ Cartório com switch.cpp
@@ -121,7 +121,8 @@ int main ()
 		//opções:
 		printf ("\t1 - Registrar nomes\n");
 		printf ("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n");
+		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf ("Opção: ");//fim do menu
 			
 		scanf ("%d", &opcao);// armazenar escolhas
@@ -142,6 +143,11 @@ int main ()
 			
 			case 3:
 			deletar ();
+			break;
+			
+			case 4:
+			printf ("Obrigado por utilizar o sistema!");
+			return 0;
 			break;
 		
 			
